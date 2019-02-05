20d05c7d6f640324e114c09013a3118e8304e899
Grant Kazan <grantkazan@gmail.com>
2019-02-04 20:56:13 -0600

    add hardcoded search results array

--- Gemfile
+++ Gemfile
@@ -25,6 +25,9 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'jwt'
 
+# use nokogiri
+gem 'nokogiri'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/controllers/api/shows_controller.rb
+++ app/controllers/api/shows_controller.rb
@@ -1,6 +1,41 @@
 class Api::ShowsController < ApplicationController
   def read
-    render json: {message: "Hello!"}
+
+    doc = Nokogiri::HTML(open('https://instantwatcher.com/search?source=1+2+3&q=parks+and+recreation&sort=queue_count+desc&view=synopsis&infinite=on&content_type%5B%5D=1&content_type%5B%5D=2'))
+
+    arr = []
+
+    doc.css(".title-link").each do |link|
+      if link.text != ""
+        # p "TITLE: "
+        arr.push(link.text)
+        # p "STUFF: "
+        # p link.values[-1]
+        # p "---------"
+        # p "----------"
+      end
+    end
+
+
+
+
+    render json: {message: arr}
+# p doc.css(".item-group-container")
+
+#  THIS WORKS FOR SEARCH RESULTS!
+# also gets the value that you'll need to get the season ID for URL
+    # doc.css(".title-link").each do |link|
+    #   if link.text != ""
+    #     p "TITLE: "
+    #     p link.text
+    #     p "STUFF: "
+    #     p link.values[-1]
+    #     p "---------"
+    #     p "----------"
+    #   end
+    # end
+
+
   end
 
 end
