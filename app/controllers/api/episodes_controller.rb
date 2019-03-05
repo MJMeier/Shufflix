5200657497ff1d504e3c69aa8f4572e898f9131c
Grant Kazan <grantkazan@gmail.com>
2019-03-04 20:19:15 -0600

    shuffle season grabs random episode html

--- app/controllers/api/episodes_controller.rb
+++ app/controllers/api/episodes_controller.rb
@@ -1,28 +1,24 @@
 class Api::EpisodesController < ApplicationController
 
-  # ids to use. will be sent over from FE
-  # 80021755
-  # 80067702
-  # 80170693
 
   def random
     selection = params[:id]
 
     season = Nokogiri::HTML(open("https://instantwatcher.com/title/" + selection + "?source=1%202%203&view=synopsis&infinite=on&_pjax=%23filters-plus-results&content_type=1%202"))
 
-    # THIS GETS ALL EPISODE TITLES AND EPISODE COUNT!
+
     count = 0
-    season.css(".episode-title").each do
-      # p node.text
+    blocks = []
+    season.css(".iw-title.netflix-title.list-title").each do |node|
+      blocks<<node.to_html
       count += 1
     end
-    # p "TOTAL EPISODES:"
-    # p count
-
+    
     prng = Random.new
     ep = prng.rand(1..count)
+    selection = blocks[ep]
 
-    render json: { "watch episode": ep }
+    render json: { watch: selection }
 
   end
 
